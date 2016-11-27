90cb9af6977829cbe312c2aac51850631c3cde73
Shen Wang <swang05@uw.edu>
2016-11-27 14:34:25 -0800

    move method

--- src/mine_player.java
+++ src/mine_player.java
@@ -21,53 +21,20 @@ public class mine_player {
 	public static int player_id;
 	public static boolean madeMove = false;  // can be replaced by checking if color is mycolor
 	public static int[] mycolor;
-	
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter host name");
-		String hostname = scan.nextLine();
-		InetAddress IPAddress = InetAddress.getByName(hostname);
-		int port_num = scan.nextInt();
-		// connect the server 
-		Socket socket = new Socket(IPAddress, port_num);
-		int error_count = 0;
-		int max_error = 100;
-		while(true) {
-			if(socket.isConnected()) {
-				break;
-			} else {
-				error_count++;
-				if(error_count == max_error) {
-					socket.close();
-					System.out.println("Failed");
-				}
-			}
-		}
-		// read server packet
-		InputStream in = socket.getInputStream();
-		DataInputStream dis = new DataInputStream(in);
-		byte[] data = new byte[48];
-		dis.readFully(data);
-		// if failed quit
-		if (!handleFirstPacket(decodePacket(ByteBuffer.wrap(data)))) return;
-		
-	}
+
 	
 	// handles the server's initial packet
 	// ia[0] is ack
 	// ia[1] is size
 	// ia[2],ia[3],ia[4] is color
 	// ia[5] is color
-	public static boolean handleFirstPacket(int[] ia) {
+	// return whether user servive
+	public static int handleFirstPacket(int[] ia) {
 		if (ia[0] == 0) {  // failed to join
 			System.out.println("Fail to join the game!");
 			// disconnect
-			return false;
+			//return false;
+
 		} else {  // success
 			mycolor = new int[3];
 			// get color
@@ -77,31 +44,15 @@ public class mine_player {
 			// get id
 			player_id = ia[5];
 			// draw the GUI
-			createPanel(ia[1]);
-			return true;
+			//createPanel(ia[1]);
+			//return true;
+
 		}
+		return ia[0];
 	}
+
 	
-	// handles server's response
-	// ia[0] is ack: whether a client is dead or not
-	// ia[1], ia[2]: coordinate
-	// ia[3], ia[4], ia[5] are colors
-	public static void handleServerPacket(int[] ia) {
-		if (ia[0] == -1) {  // error case, do nothing
-			return;
-		} else if (ia[0] == 0) {  // explode case
-			setColor(ia[1], ia[2], new int[]{ia[3], ia[4], ia[5]});
-			if (madeMove) {  // it was this user who exploded, disconnect
-				System.out.println("YOU EXPLODED");
-				// disconnect
-				return;
-			}
-		} else {
-			setColor(ia[1], ia[2], new int[]{ia[3], ia[4], ia[5]});
-			madeMove = false;  // move completed
-		}
-	}
-	
+
 	// get the request and prepare the packet
 	public static byte[] action(int row, int col) {
 		ByteBuffer bb = ByteBuffer.allocate(24);
@@ -109,7 +60,7 @@ public class mine_player {
 		madeMove = true;  // it made a move, set it to true
 		return bb.array();
 	}
-	
+
 	// decode the server packet
 	public static int[] decodePacket(ByteBuffer bb) {
 		int[] res = new int[6];
@@ -118,15 +69,7 @@ public class mine_player {
 		}
 		return null;
 	}
+
 	
-	// utility function to set color to [row, col]
-	public static void setColor(int row, int col, int[] color) {
-		
-	}
-	
-	// utility function to create an actual GUI on client screen
-	public static void createPanel(int size) {
-		// create a graphical grid
-	}
 
 }
--- src/mineboard.java
+++ src/mineboard.java
@@ -25,79 +25,141 @@ public class mineboard {
 	private JButton[][] buttons;
 	private int size;
 	private JPanel mine_board;
-	
-	
-	mineboard(int size){
+	public Socket socket;
+
+
+	mineboard(int size, String hostname, int port_num){
+		InetAddress IPAddress = InetAddress.getByName(hostname);
 		this.buttons = new JButton[size][size];
 		this.size = size;
-        initializeGui();
-    }
+		// connect the server 
+		this.socket = new Socket(IPAddress, port_num);
+		int error_count = 0;
+		int max_error = 100;
+		while(true) {
+			if(socket.isConnected()) {
+				break;
+			} else {
+				error_count++;
+				if(error_count == max_error) {
+					socket.close();
+					System.out.println("Failed");
+				}
+			}
+		}
+		// read server packet
+		InputStream in = socket.getInputStream();
+		DataInputStream dis = new DataInputStream(in);
+		byte[] data = new byte[48];
+		dis.readFully(data);
+		// if failed quit
+		//if (!handleFirstPacket(decodePacket(ByteBuffer.wrap(data)))) return;
+		initializeGui();
+	}
 
 	public final void initializeGui() {
-		
+
 		mine_board = new JPanel() {
-            /**
+			/**
 			 * 
 			 */
 			private static final long serialVersionUID = 1L;
 
 			/**
-             * Override the preferred size to return the largest it can, in
-             * a square shape.  Must (must, must) be added to a GridBagLayout
-             * as the only component (it uses the parent as a guide to size)
-             * with no GridBagConstaint (so it is centered).
-             */
-            @Override
-            public final Dimension getPreferredSize() {
-                Dimension d = super.getPreferredSize();
-                Dimension prefSize = null;
-                Component c = getParent();
-                if (c == null) {
-                    prefSize = new Dimension(
-                            (int)d.getWidth(),(int)d.getHeight());
-                } else if (c!=null &&
-                        c.getWidth()>d.getWidth() &&
-                        c.getHeight()>d.getHeight()) {
-                    prefSize = c.getSize();
-                } else {
-                    prefSize = d;
-                }
-                int w = (int) prefSize.getWidth();
-                int h = (int) prefSize.getHeight();
-                // the smaller of the two sizes
-                int s = (w>h ? h : w);
-                return new Dimension(s,s);
-            }
-        };
-        
-        mine_board.setBorder(new CompoundBorder(
-                new EmptyBorder(size,size,size,size),
-                new LineBorder(Color.BLACK)
-                ));
-        
-        mine_board.setBackground(new Color(255,255,255));
-        JPanel boardConstrain = new JPanel(new GridBagLayout());
-        boardConstrain.setBackground(new Color(255,255,255));
-        boardConstrain.add(mine_board);
-        gui.add(boardConstrain);
-		
-        // create the mine board squares
-        Insets buttonMargin = new Insets(0, 0, 0, 0);
-        for (int ii = 0; ii < buttons.length; ii++) {
-            for (int jj = 0; jj < buttons[ii].length; jj++) {
-                JButton b = new JButton();
-                b.setMargin(buttonMargin);
-                buttons[jj][ii] = b;
-            }
-        }
-        
+			 * Override the preferred size to return the largest it can, in
+			 * a square shape.  Must (must, must) be added to a GridBagLayout
+			 * as the only component (it uses the parent as a guide to size)
+			 * with no GridBagConstaint (so it is centered).
+			 */
+			@Override
+			public final Dimension getPreferredSize() {
+				Dimension d = super.getPreferredSize();
+				Dimension prefSize = null;
+				Component c = getParent();
+				if (c == null) {
+					prefSize = new Dimension(
+							(int)d.getWidth(),(int)d.getHeight());
+				} else if (c!=null &&
+						c.getWidth()>d.getWidth() &&
+						c.getHeight()>d.getHeight()) {
+					prefSize = c.getSize();
+				} else {
+					prefSize = d;
+				}
+				int w = (int) prefSize.getWidth();
+				int h = (int) prefSize.getHeight();
+				// the smaller of the two sizes
+				int s = (w>h ? h : w);
+				return new Dimension(s,s);
+			}
+		};
+
+		mine_board.setBorder(new CompoundBorder(
+				new EmptyBorder(size,size,size,size),
+				new LineBorder(Color.BLACK)
+				));
+
+		mine_board.setBackground(new Color(255,255,255));
+		JPanel boardConstrain = new JPanel(new GridBagLayout());
+		boardConstrain.setBackground(new Color(255,255,255));
+		boardConstrain.add(mine_board);
+		gui.add(boardConstrain);
+
+		// create the mine board squares
+		Insets buttonMargin = new Insets(0, 0, 0, 0);
+		for (int ii = 0; ii < buttons.length; ii++) {
+			for (int jj = 0; jj < buttons[ii].length; jj++) {
+				JButton b = new JButton();
+				b.setMargin(buttonMargin);
+				buttons[jj][ii] = b;
+			}
+		}
+
 	}
+
+	// handles server's response
+	// ia[0] is ack: whether a client is dead or not
+	// ia[1], ia[2]: coordinate
+	// ia[3], ia[4], ia[5] are colors
+	public static void handleServerPacket(int[] ia) {
+		if (ia[0] == -1) {  // error case, do nothing
+			return;
+		} else if (ia[0] == 0) {  // explode case
+			setColor(ia[1], ia[2], new int[]{ia[3], ia[4], ia[5]});
+			if (madeMove) {  // it was this user who exploded, disconnect
+				System.out.println("YOU EXPLODED");
+				// disconnect
+				return;
+			}
+		} else {
+			setColor(ia[1], ia[2], new int[]{ia[3], ia[4], ia[5]});
+			madeMove = false;  // move completed
+		}
+	}
+
+	// utility function to set color to [row, col]
+	public static void setColor(int row, int col, int[] color) {
+
+	}
+
+	// utility function to create an actual GUI on client screen
+	public static void createPanel(int size) {
+		// create a graphical grid
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter host name");
+		String hostname = scan.nextLine();
+		int port_num = scan.nextInt();
+
+
+
+
+
 	}
 
 }
