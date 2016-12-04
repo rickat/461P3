2832a452e1f67bb9f1f9a1640697c94f9e70fdde
ylh96 <ylh96@10.0.0.243>
2016-12-03 18:32:40 -0800

    a

--- src/mine_board_mg.java
+++ src/mine_board_mg.java
@@ -2,15 +2,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.InetAddress;
-import java.net.Socket;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.util.Scanner;
+import java.nio.channels.SocketChannel;
 
 import javax.swing.*;
 import javax.swing.border.*;
@@ -25,8 +21,8 @@ public class mine_board_mg extends Frame implements ActionListener{
     private JPanel mineBoard;
     private int size; // = 25;  // HERE
     private static JButton[][] buttons; // = new JButton[size][size];  // HERE
-    public static Socket socket;
-    public static Socket game_socket;
+    public static SocketChannel socket;
+    public static SocketChannel game_socket;
 	public static MinePlayerClass mp;
 	public static int[] change_color = new int[3];
 
@@ -34,7 +30,8 @@ public class mine_board_mg extends Frame implements ActionListener{
     	InetAddress IPAddress = InetAddress.getByName(hostname);
 
 		// connect the server 
-		socket = new Socket(IPAddress, port_num);
+		socket = SocketChannel.open(); 
+		socket.connect(new InetSocketAddress(IPAddress, port_num));
 		int error_count = 0;
 		int max_error = 100;
 		while(true) {
@@ -52,16 +49,15 @@ public class mine_board_mg extends Frame implements ActionListener{
 		System.out.println("start initialize mp");
 		mp = new MinePlayerClass();
 		// read server packet
-		InputStream in = socket.getInputStream();
-		DataInputStream dis = new DataInputStream(in);
-		byte[] data = new byte[8];
-		dis.readFully(data);
+		ByteBuffer data = ByteBuffer.allocate(8);
+		socket.read(data);
 		System.out.println("finish read");
-		int socket_num = ByteBuffer.wrap(data).getInt();
+		int socket_num = data.getInt(0);
 		System.out.println("socket num: " + socket_num);
 		// if failed quit
 		if (socket_num == -1) System.exit(ABORT);
-		mine_board_mg.game_socket = new Socket(IPAddress, socket_num);
+		mine_board_mg.game_socket = SocketChannel.open(); 
+		game_socket.connect(new InetSocketAddress(IPAddress, socket_num));
 		error_count = 0;
 		while(true) {
 			if(game_socket.isConnected()) {
@@ -75,18 +71,22 @@ public class mine_board_mg extends Frame implements ActionListener{
 				}
 			}
 		}
-		InputStream gin = game_socket.getInputStream();
-		@SuppressWarnings("unused")
-		DataInputStream gdis = new DataInputStream(gin);
-		byte[] gdata = new byte[48];
-		dis.readFully(gdata);
-		if ((this.size = mp.handleFirstPacket(mp.decodePacket(ByteBuffer.wrap(gdata)))) == -1) System.exit(ABORT);;
+		System.out.println(game_socket.isConnected());
+		ByteBuffer gdata = ByteBuffer.allocate(48);
+		game_socket.read(gdata);
+		int[] iah = mp.decodePacket(gdata);
+		for (int i = 0; i < iah.length; i++) {
+			System.out.println(iah[i]);
+		}
+		if ((this.size = mp.handleFirstPacket(iah)) == -1) System.exit(ABORT);;
 		mine_board_mg.buttons = new JButton[size][size];
+		System.out.println(size);
         initializeGui();
     }
     
     public void initializeGui() {
         // set up the main GUI
+    	System.out.println("inside initialize gui");
         gui.setBorder(new EmptyBorder(5, 5, 5, 5));
         // gui.add(new JLabel("?"), BorderLayout.LINE_START);
 
@@ -113,7 +113,7 @@ public class mine_board_mg extends Frame implements ActionListener{
                 mineBoard.add(buttons[ii][jj]);
             }
         }
-        
+        System.out.println("end initialization");
         
     }
 
@@ -146,9 +146,9 @@ public class mine_board_mg extends Frame implements ActionListener{
  			return false;
  		} else if (ia[0] == 0) {  // explode case
  			setColor(ia[1], ia[2], new int[]{ia[3], ia[4], ia[5]});
- 			if (ia[3] == mine_player.mycolor[0]
- 					&& ia[4] == mine_player.mycolor[1]
- 					&& ia[5] == mine_player.mycolor[2]) {  // it was this user who exploded, disconnect
+ 			if (ia[3] == MinePlayerClass.mycolor[0]
+ 					&& ia[4] == MinePlayerClass.mycolor[1]
+ 					&& ia[5] == MinePlayerClass.mycolor[2]) {  // it was this user who exploded, disconnect
  				System.out.println("YOU EXPLODED");
  				return false;
  			}
@@ -167,11 +167,10 @@ public class mine_board_mg extends Frame implements ActionListener{
 
  	public static void readFromServer() throws IOException {
  		while (true) {
- 			InputStream in = game_socket.getInputStream();
- 			DataInputStream dis = new DataInputStream(in);
- 			byte[] data = new byte[48];
- 			dis.readFully(data);
- 			int[] a = mp.decodePacket(ByteBuffer.wrap(data));
+ 			System.out.println("read from server");
+ 			ByteBuffer data = ByteBuffer.allocate(48);
+ 			game_socket.read(data);
+ 			int[] a = mp.decodePacket(data);
  			if (!handleServerPacket(a)) break;
  		}
  		game_socket.close();
@@ -198,6 +197,7 @@ public class mine_board_mg extends Frame implements ActionListener{
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
+					System.out.println("abort");
 					System.exit(ABORT);
 				}
 
@@ -212,6 +212,7 @@ public class mine_board_mg extends Frame implements ActionListener{
                 // ensures the minimum size is enforced.
                 f.setMinimumSize(f.getSize());
                 f.setVisible(true);
+                System.out.println("done setting up gui");
                 try {
 					readFromServer();
 				} catch (IOException e) {
@@ -232,10 +233,7 @@ public class mine_board_mg extends Frame implements ActionListener{
 					try{
 						byte[] res = mp.action(row, col);
 						// scoket send packet to server
-						OutputStream out = game_socket.getOutputStream();
-						@SuppressWarnings("unused")
-						DataOutputStream dos = new DataOutputStream(out);
-						out.write(res);
+						game_socket.write(ByteBuffer.wrap(res));
 						done = true;
 						break;
 					}catch(IOException i) {
@@ -247,4 +245,5 @@ public class mine_board_mg extends Frame implements ActionListener{
 		}
 	}
 }
+
 
